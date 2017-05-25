6f2311cd2d8f3ed08be5e635a83ad6fd72d8af7e
scanner-darkly <scannerdarkly.git@gmail.com>
2017-05-24 20:44:58 -0700

    update libavr32

--- module/main.c
+++ module/main.c
@@ -317,7 +317,7 @@ void handler_MscConnect(int32_t data) {
     assign_msc_event_handlers();
 
     // disable timers
-    irqs_pause();
+    irqflags_t irq_flags = irqs_pause();
 
     // clear screen
     for (size_t i = 0; i < 8; i++) {
@@ -331,7 +331,7 @@ void handler_MscConnect(int32_t data) {
     // renable teletype
     set_mode(M_LIVE);
     assign_main_event_handlers();
-    irqs_resume();
+    irqs_resume(irq_flags);
 }
 
 void handler_Trigger(int32_t data) {
