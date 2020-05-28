4f763cdfcb2a6d0741f8fd33813c63311658e9f3
scanner-darkly <scannerdarkly.git@gmail.com>
2020-05-27 21:12:33 -0700

    disting ex ops

---
+++ src/ops/disting.h
@@ -0,0 +1,52 @@
+#ifndef _OPS_DISTING_H_
+#define _OPS_DISTING_H_
+
+#include "ops/op.h"
+
+extern const tele_op_t op_EX_1;
+extern const tele_op_t op_EX_2;
+extern const tele_op_t op_EX_3;
+extern const tele_op_t op_EX_4;
+extern const tele_op_t op_EX_LOAD;
+extern const tele_op_t op_EX_L;
+extern const tele_op_t op_EX_SAVE;
+extern const tele_op_t op_EX_S;
+extern const tele_op_t op_EX_RESET;
+extern const tele_op_t op_EX_ALG;
+extern const tele_op_t op_EX_A;
+extern const tele_op_t op_EX_CTRL;
+extern const tele_op_t op_EX_C;
+extern const tele_op_t op_EX_PARAM;
+extern const tele_op_t op_EX_P;
+extern const tele_op_t op_EX_REC;
+extern const tele_op_t op_EX_PLAY;
+extern const tele_op_t op_EX_AL_P;
+extern const tele_op_t op_EX_AL_CLK;
+extern const tele_op_t op_EX_M_CH;
+extern const tele_op_t op_EX_M_N;
+extern const tele_op_t op_EX_M_O;
+extern const tele_op_t op_EX_M_PB;
+extern const tele_op_t op_EX_M_CC;
+extern const tele_op_t op_EX_M_PRG;
+extern const tele_op_t op_EX_M_CLK;
+extern const tele_op_t op_EX_M_START;
+extern const tele_op_t op_EX_M_STOP;
+extern const tele_op_t op_EX_M_CONT;
+extern const tele_op_t op_EX_SB_CH;
+extern const tele_op_t op_EX_SB_N;
+extern const tele_op_t op_EX_SB_O;
+extern const tele_op_t op_EX_SB_PB;
+extern const tele_op_t op_EX_SB_CC;
+extern const tele_op_t op_EX_SB_PRG;
+extern const tele_op_t op_EX_SB_CLK;
+extern const tele_op_t op_EX_SB_START;
+extern const tele_op_t op_EX_SB_STOP;
+extern const tele_op_t op_EX_SB_CONT;
+extern const tele_op_t op_EX_VOX_P;
+extern const tele_op_t op_EX_VP;
+extern const tele_op_t op_EX_VOX;
+extern const tele_op_t op_EX_V;
+extern const tele_op_t op_EX_VOX_O;
+extern const tele_op_t op_EX_VO;
+
+#endif
--- src/ops/op.c
+++ src/ops/op.c
@@ -8,6 +8,7 @@
 #include "ops/ansible.h"
 #include "ops/controlflow.h"
 #include "ops/delay.h"
+#include "ops/disting.h"
 #include "ops/earthsea.h"
 #include "ops/er301.h"
 #include "ops/fader.h"
@@ -218,6 +219,16 @@ const tele_op_t *tele_ops[E_OP__LENGTH] = {
     &op_MA_SELECT, &op_MA_STEP, &op_MA_RESET, &op_MA_PGM, &op_MA_ON, &op_MA_PON,
     &op_MA_OFF, &op_MA_POFF, &op_MA_SET, &op_MA_PSET, &op_MA_COL, &op_MA_PCOL,
     &op_MA_ROW, &op_MA_PROW, &op_MA_CLR, &op_MA_PCLR,
+
+    // disting ex
+    &op_EX_1, &op_EX_2, &op_EX_3, &op_EX_4, &op_EX_LOAD, &op_EX_L, &op_EX_SAVE,
+    &op_EX_S, &op_EX_RESET, &op_EX_ALG, &op_EX_A, &op_EX_CTRL, &op_EX_C,
+    &op_EX_PARAM, &op_EX_P, &op_EX_REC, &op_EX_PLAY, &op_EX_AL_P, &op_EX_AL_CLK,
+    &op_EX_M_CH, &op_EX_M_N, &op_EX_M_O, &op_EX_M_PB, &op_EX_M_CC, &op_EX_M_PRG,
+    &op_EX_M_CLK, &op_EX_M_START, &op_EX_M_STOP, &op_EX_M_CONT, &op_EX_SB_CH,
+    &op_EX_SB_N, &op_EX_SB_O, &op_EX_SB_PB, &op_EX_SB_CC, &op_EX_SB_PRG,
+    &op_EX_SB_CLK, &op_EX_SB_START, &op_EX_SB_STOP, &op_EX_SB_CONT,
+    &op_EX_VOX_P, &op_EX_VP, &op_EX_VOX, &op_EX_V, &op_EX_VOX_O, &op_EX_VO,
 
     // seed
     &op_SEED, &op_RAND_SEED, &op_SYM_RAND_SD, &op_SYM_R_SD, &op_TOSS_SEED,
